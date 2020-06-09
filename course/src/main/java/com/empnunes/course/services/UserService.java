1bd35961be3c948cced11811b044aec7b432acc3
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-08 22:18:20 -0300

    Service layer, component registration

---
+++ course/src/main/java/com/empnunes/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.empnunes.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.empnunes.course.entities.User;
+import com.empnunes.course.repositories.UserRepository;
+
+// preciso identificar(registrando como um component) minha classe pra ser possivel
+// injeta-lo em outra classe e devolver o resultado em um response
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		// findbyid retorna um optional
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
