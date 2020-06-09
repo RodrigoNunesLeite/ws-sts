93fd330e0dd83505adf7fbec502de9bde8833628
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-08 21:04:55 -0300

    User entity and resource

---
+++ course/src/main/java/com/empnunes/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.empnunes.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.empnunes.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L,"Maria","maria@gmail.com","11-1023901293","12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
