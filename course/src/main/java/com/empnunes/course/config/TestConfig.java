e89985a8c68020cf6a517b3830dcd2e1e63431b7
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-09 23:08:25 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- course/src/main/java/com/empnunes/course/config/TestConfig.java
+++ course/src/main/java/com/empnunes/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.empnunes.course.entities.Category;
 import com.empnunes.course.entities.Order;
+import com.empnunes.course.entities.OrderItem;
 import com.empnunes.course.entities.Product;
 import com.empnunes.course.entities.User;
 import com.empnunes.course.entities.enums.OrderStatus;
 import com.empnunes.course.repositories.CategoryRepository;
+import com.empnunes.course.repositories.OrderItemRepository;
 import com.empnunes.course.repositories.OrderRepository;
 import com.empnunes.course.repositories.ProductRepository;
 import com.empnunes.course.repositories.UserRepository;
@@ -39,6 +41,9 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
+	
 	// executa quando a função é iniciada
 	@Override
 	public void run(String... args) throws Exception {
@@ -53,16 +58,7 @@ public class TestConfig implements CommandLineRunner{
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
 		
-		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
-		
-		// salva uma lista de objetos no banco
-		userRepository.saveAll(Arrays.asList(u1,u2));
-		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
@@ -76,6 +72,25 @@ public class TestConfig implements CommandLineRunner{
 		
 		// salvando os produtos com as associações
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
+		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1); 
+
+		// salva uma lista de objetos no banco
+		userRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
 		
 	}
 }
