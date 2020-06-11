2614297e25eb3fe6785fa13719cbc4680f1cf80b
Rodrigo Nunes <rodrigonl10.rn@gmail.com>
2020-06-11 00:30:48 -0300

    User delete

--- course/src/main/java/com/empnunes/course/resources/UserResource.java
+++ course/src/main/java/com/empnunes/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -46,5 +47,13 @@ public class UserResource {
 		 * mas pra isso, preciso passar uma location, definida acima
 		 * */
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		/* noContent() = para retorno de respostas vazias, codigo 204
+		 * */
+		return ResponseEntity.noContent().build();
 	}
 }
